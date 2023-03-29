12c5312aacebd1c1a891813e825d1b7cacd454b6
Mayerlin Rojas Perez <mrojas2804@misena.edu.co>
2023-03-28 22:35:19 -0500

    Se agrega rama dev

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.scss';
+import Header from './components/header/header';
+import Main from './components/main/main';
+import Footer from './components/footer/footer';
+
+function App() {
+  return (
+    <div className='App'>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
